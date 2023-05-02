6c3aff497fe305a95180da11b14de3cc5e0c393d
Melki <ymelki@gmail.com>
2023-05-02 16:40:25 +0200

    select aeroport

---
+++ aeroport.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+// 1 connexion à la B.D.
+$dsn = 'mysql:dbname=reservation;host=127.0.0.1';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+
+   //2. RECUPERER LES DONNEES 
+   $resultat = $dbh->query("select * from aeroports")->fetchAll();
+// 3. AFFICHAGE DES DONNES
+// var_dump($resultat);
+
+?>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+<?php foreach ($resultat as $unaeroport) {  ?>
+
+    <div class="col">
+        <div class="card h-100">
+        <div class="card-body">
+            <h5 class="card-title"><?=$unaeroport['Nom_aeroport'] ?></h5>
+            <p class="card-text">
+                <?=$unaeroport['Code_aeroport'] ?>  - 
+                <?=$unaeroport['Pays'] ?> - 
+                <?=$unaeroport['Ville'] ?>
+        </div>
+        </div>
+    </div> 
+
+  
+
+<?php } ?>
+</div>
+</body>
+</html>
